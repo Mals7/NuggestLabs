01888b7943ba2902628b1445ad628c0109fa224c
Mals7 <ruizgonzalo7@hotmail.com>
2016-03-01 20:32:51 -0400

    Added files via upload

---
+++ PaginaWeb/juegos.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
+
+Name       : StoneWork 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20140228
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/index.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<link href="http://fonts.googleapis.com/css?family=Didact+Gothic" rel="stylesheet" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+
+<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<title>Bootstrap NavBar</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</head>
+<body style="background-color:#000">
+<div class="contenedor">
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Nuggets Lab</a>
+    </div>
+    <div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="Inicio.php">INICIO</a></li>
+        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Juegos <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="juegos.php">PC</a></li>
+            <li><a href="#">Consola</a></li>
+            <li><a href="#">Arcade</a></li>
+          </ul>
+        </li>
+        <li><a href="descargas.php">Descargas</a></li>
+        <li><a href="#">Noticias</a></li>
+        <li><a href="#">Contactanos</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+<div id="header-wrapper">
+  <div id="banner" class="container">
+    <div class="title">
+	    <div id="banner2" class="container">
+	      <div class="title">
+	        <h2>
+	          <mark style="background-color:#FF0">BASE GAMER</mark>
+            </h2>
+	        <span class="byline"><b>
+	          <mark style="background-color:#FF0">EL SITIO QUE TODO GAMER BUSCA</mark>
+	          </b></span></div>
+        </div>
+    </div>
+  </div>
+</div>
+</div>
+<div>
+<br>
+
+  <!-- InstanceBeginEditable name="Contenido" -->
+    <h1 class="fa-stack-2x">Juegos</h1>
+    <p>&nbsp;</p>
+    <form>
+    <div style="float:left; margin-left:20px; padding-right:10px;"><a href="juegos/hon.php"><img src="images/HoN.png" width="204" height="176" /></a></div>
+    <div>
+    <h1> Heroes of Newerth</h1>
+    <p style="color:#FFF; margin-right:20px; padding-left:10px;"> Heroes of Newerth (HoN) es un juego multiplayer online estilo MOBA 
+    (Multiplayer Online Battle Arena) originalmente desarrollado por S2 Games para los sistemas operativos Microsoft Windows, Mac OS X y Linux.<br>
+    La idea del juego se deriva de Warcraft III: The Frozen Throne, siendo el primer juego MOBA de S2 Games. El juego fue lanzado el 12 de Mayo del 2010 y re-lanzado en modo gratis el 29 de Julio del 2011. En Mayo del 2015 se anunció que fue adquirido por Frostburn Studios.</p>
+    <p><a href="juegos/hon.php"> Ver mas...</a></p>
+    </div>
+    <hr style="margin-bottom:20px; margin-top:50px;"/>
+    <div style="float:right; margin-right:20px; padding-left:10px;"><a href="juegos/dota.php"><img src="images/Dota1.png"/></a></div>
+    <div>
+    <h1 style="color:#FFF; margin-left:20px;"> DotA </h1>
+    <p style="color:#FFF; margin-left:20px; padding-right:10px;"> Defense of the Ancients (denominado por sus siglas como DotA) es un escenario personalizado para el videojuego Warcraft III: The Frozen Throne desarrollado por Blizzard Entertainment, creado con el Editor del Mundo que incluye el videojuego. El mapa se juega en dos equipos, y el objetivo del juego es destruir el Ancient (en español «ancestro») del oponente. Los Ancients son estructuras fuertemente protegidas, ubicadas en esquinas opuestas del mapa. Los jugadores controlan poderosas unidades conocidas como "héroes", que son ayudadas por unidades controladas por inteligencia artificial llamadas "creeps". Como en un videojuego de rol, los héroes obtienen puntos de experiencia para incrementar sus habilidades y usan oro para comprar equipo y elementos útiles durante la misión.<br>La popularidad de DotA hizo que más adelante se produjeran juegos con un estilo similar. Además, en octubre de 2010 Valve Corporation anunció la realización de una secuela: Dota 2</p>
+    <p style="color:#FFF; margin-left:20px;"> <a href="juegos/dota.php">Ver mas...</a></p>
+    </div>
+    <hr style="margin-bottom:20px; margin-top:50px;"/>
+    <div style="float:left; margin-left:20px; padding-right:10px;"><a href="juegos/dota2.php"><img src="images/Dota2.png" width="340" height="269"/></a></div>
+    <div>
+    <h1>DOTA 2</h1>
+    <p style="color:#FFF; margin-right:20px; padding-left:10px;">Dota 2 es un videojuego del género ARTS, también conocido como MOBA, lanzado el 9 de julio de 2013. El juego fue desarrollado por Valve Corporation. Es un juego matemático y físico para la mente es mejor jugar tres veces a la semana las neuronas se desarrollan. distribuido por la universidad mayor de San marcos Bazz para Warcraft III: The Frozen Throne. Entró en su etapa Beta a principios del 2011, y finalmente se lanzó al público en general a través de Steam el 9 de julio de 2013 para Microsoft Windows, y el 18 de julio de 2013 para OS X y Linux, el 17 de junio del 2015 Valve lanzo la beta del juego usando el nuevo motor gráfico Source 2 [Dota 2 Reborn]. Actualmente es el juego más jugado de Steam con más de 11.6 millones de jugadores.<br>Valve Corporation organiza de manera anual el campeonato mundial The International, que en su primera edición fue jugado en Colonia, Alemania, en el 2011. La edición 2014 en Seattle, Estados Unidos, repartió 10.927.499 US$, y en su edición 2015 repartió 18.429.613 US$.</p>
+  <p><a href="juegos/dota2.php"> Ver mas...</a></p>
+  </div>
+   <hr style="margin-bottom:20px; margin-top:50px;"/>
+   <div style="float:right; margin-right:20px; padding-left:10px;"><a href="juegos/lol.php"><img src="images/Lol.png"/></a></div>
+  <h1 style="color:#FFF; margin-left:20px;">League of Legends</h1>
+   <div>
+   <p style="color:#FFF; margin-left:20px; padding-right:10px;"> League of Legends (también conocido por sus siglas LoL) es un videojuego de género multiplayer online battle arena (MOBA) desarrollado por Riot Games para Microsoft Windows y OS X. En un principio los servidores estaban alojados en la empresa GOA y hace poco se anunció su venta a la empresa china Tencent Holdings Limited. El 7 de octubre del 2008 fue anunciado su lanzamiento para Alistar, para septiembre del 2009. Finalmente el juego entró a beta cerrada el 10 de abril de ese año. El juego fue diseñado para el sistema operativo Microsoft Windows y lanzaron una beta para OS X dejando al poco tiempo el proyecto de lado. Esta beta ha sido mejorada por parte de usuarios del juego, haciendo posible jugar a una versión actualizada de League of Legends. También es posible jugarlo en el sistema operativo GNU/Linux a través de aplicaciones de terceros. En octubre del 2012, contaba con 70 millones de jugadores registrados. Para marzo del 2013 se registró un pico de 5 millones de jugadores conectados al mismo tiempo en todo el mundo. Es un juego de alta competitividad siendo uno de los juegos más populares de los deportes electrónicos (e-sports).</p>
+   <p style="color:#FFF; margin-left:20px;"><a href="juegos/lol.php"> Ver mas...</a></p>
+   </div>
+    
+    </form>
+    <p>&nbsp;</p>
+  
+<title></title>
+
+<!-- InstanceEndEditable -->
+
+<br><br>
+</div>
+
+		<div class="boxA">
+			<span class="fa fa-cloud-download"></span>
+			<p>Descarga de juegos.</p>
+			<a href="#" class="button button-alt">Entrar</a>
+		</div>
+		<div class="boxB">
+			<span class="fa fa-file-text"></span>
+			<p>Informacion sobre juegos.</p>
+			<a href="juegos.php" class="button button-alt">Entrar</a>
+		</div>
+		<div class="boxC">
+			<span class="fa fa-globe"></span>
+			<p> Novedades.</p>
+			<a href="#" class="button button-alt">Entrar</a>
+		</div>
+        <div class="boxD">
+			<span class="fa fa-gamepad"></span>
+			<p> Gameplays y tutoriales.</p>
+			<a href="#" class="button button-alt">Entrar</a>
+		</div>
+	</div>
+</div>
+<div id="welcome">
+	<div class="container">
+		<div class="title">
+			 <h2><mark style="background-color:#FF0">BASE GAMER</mark></h2>
+			<span class="byline"><b><mark style="background-color:#FF0">EL SITIO QUE TODO GAMER BUSCA</mark>
+            
+			</b></span></div>
+
+</body>
+<!-- InstanceEnd --></html>
---
+++ PaginaWeb/juegos/dota.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
+
+Name       : StoneWork 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20140228
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/index.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<link href="http://fonts.googleapis.com/css?family=Didact+Gothic" rel="stylesheet" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+
+<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<title>Bootstrap NavBar</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</head>
+<body style="background-color:#000">
+<div class="contenedor">
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Nuggets Lab</a>
+    </div>
+    <div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="Inicio.php">INICIO</a></li>
+        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Juegos <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="juegos.php">PC</a></li>
+            <li><a href="#">Consola</a></li>
+            <li><a href="#">Arcade</a></li>
+          </ul>
+        </li>
+        <li><a href="descargas.php">Descargas</a></li>
+        <li><a href="#">Noticias</a></li>
+        <li><a href="#">Contactanos</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+<div id="header-wrapper">
+  <div id="banner" class="container">
+    <div class="title">
+	    <div id="banner2" class="container">
+	      <div class="title">
+	        <h2>
+	          <mark style="background-color:#FF0">BASE GAMER</mark>
+            </h2>
+	        <span class="byline"><b>
+	          <mark style="background-color:#FF0">EL SITIO QUE TODO GAMER BUSCA</mark>
+	          </b></span></div>
+        </div>
+    </div>
+  </div>
+</div>
+</div>
+<div>
+<br>
+
+  <!-- InstanceBeginEditable name="Contenido" -->
+  
+  <div><img src="imagenes_juegos/dota/dota_banner.png" style="margin-left:260px; margin-right:20px;" /></div>
+  <h1 class="fa-stack-2x">Defense of the Ancients
+  </h1>
+  <p>&nbsp;</p>
+  
+<title></title>
+
+<!-- InstanceEndEditable -->
+
+<br><br>
+</div>
+
+		<div class="boxA">
+			<span class="fa fa-cloud-download"></span>
+			<p>Descarga de juegos.</p>
+			<a href="#" class="button button-alt">Entrar</a>
+		</div>
+		<div class="boxB">
+			<span class="fa fa-file-text"></span>
+			<p>Informacion sobre juegos.</p>
+			<a href="../juegos.php" class="button button-alt">Entrar</a>
+		</div>
+		<div class="boxC">
+			<span class="fa fa-globe"></span>
+			<p> Novedades.</p>
+			<a href="#" class="button button-alt">Entrar</a>
+		</div>
+        <div class="boxD">
+			<span class="fa fa-gamepad"></span>
+			<p> Gameplays y tutoriales.</p>
+			<a href="#" class="button button-alt">Entrar</a>
+		</div>
+	</div>
+</div>
+<div id="welcome">
+	<div class="container">
+		<div class="title">
+			 <h2><mark style="background-color:#FF0">BASE GAMER</mark></h2>
+			<span class="byline"><b><mark style="background-color:#FF0">EL SITIO QUE TODO GAMER BUSCA</mark>
+            
+			</b></span></div>
+
+</body>
+<!-- InstanceEnd --></html>
